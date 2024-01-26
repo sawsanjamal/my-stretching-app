56adc3fceed48d9283663e570d7a91f9eee535bd
sawsanjamal <140208510+sawsanjamal@users.noreply.github.com>
2024-01-25 18:31:56 -0600

    implement human body with hover feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import { HumanFront } from "../components/humanBody/humanFront";
+import { HumanBack } from "../components/humanBody/humanBack";
 export default function Home() {
   return (
     <div className="home-container">
       <div className="human-body-container">
-        <div className="human-body-front">Front</div>
-        <div className="human-body-back">Back</div>
+        <div className="human-body-front">
+          <HumanFront />
+        </div>
+        <div className="human-body-back">
+          <HumanBack />
+        </div>
       </div>
       <div className="articles-container">
         <div className="article">Article 1</div>
